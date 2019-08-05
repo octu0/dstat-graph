51177172d5a22e8f28cee055a2dc622b77bf23b1
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2019-08-06 00:14:10 +0900

    rename pkg

--- examples/out.sh
+++ examples/out.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-GOPATH="/Users/yusuke.hata/go"
-go run $GOPATH/src/github.com/octu0/dstat-graph.go/cmd/main.go --csv ./dstat.csv -o cpu.png -f usr,sys,idl,wai
-go run $GOPATH/src/github.com/octu0/dstat-graph.go/cmd/main.go --csv ./dstat.csv -o traf.png -f recv,send
-go run $GOPATH/src/github.com/octu0/dstat-graph.go/cmd/main.go --csv ./dstat.csv -o intr.png -f int,csw
-go run $GOPATH/src/github.com/octu0/dstat-graph.go/cmd/main.go --csv ./dstat.csv -o net.png -f lis,act,syn,tim,clo
+go run $GOPATH/src/github.com/octu0/dstat-graph/cmd/main.go --csv ./dstat.csv -o cpu.png -f usr,sys,idl,wai
+go run $GOPATH/src/github.com/octu0/dstat-graph/cmd/main.go --csv ./dstat.csv -o traf.png -f recv,send
+go run $GOPATH/src/github.com/octu0/dstat-graph/cmd/main.go --csv ./dstat.csv -o intr.png -f int,csw
+go run $GOPATH/src/github.com/octu0/dstat-graph/cmd/main.go --csv ./dstat.csv -o net.png -f lis,act,syn,tim,clo
