4d1d265da6e008d661dedefeab315f0b55cd7994
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2020-12-09 14:05:12 +0900

    go fmt

--- chart.go
+++ chart.go
@@ -1,18 +1,18 @@
 package graph
 
-import(
-  "io"
-  "os"
-  "time"
+import (
+	"io"
+	"os"
+	"time"
 )
 
 type Chart interface {
-  Read(io.Reader) error
-  RenderToFile(*os.File) error
+	Read(io.Reader) error
+	RenderToFile(*os.File) error
 }
 
 type DstatRecord map[string]float64
 type DstatCSVRow struct {
-  Time      time.Time
-  Values    DstatRecord
+	Time   time.Time
+	Values DstatRecord
 }
